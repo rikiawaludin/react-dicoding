5e2c10fc98738d6f07efedcda6c4c1aa55432d53
Riki Awaludin <rikiawaludin17@gmail.com>
2024-12-25 11:54:55 +0700

    react component with function

---
+++ src/Profile.jsx
@@ -0,0 +1,20 @@
+function Profile() {
+  return (
+    <>
+      <div>
+        <h1>Biodata Perusahaan</h1>
+        <ul>
+          <li>Nama: Dicoding Indonesia</li>
+          <li>Bidang: Education</li>
+          <li>Tagline: Decode Ideas, Discover Potential.</li>
+          <img
+            src="https://raw.githubusercontent.com/dicodingacademy/BelajarDasarPemrogramanWeb/099-shared-files/dicoding-logo.png"
+            alt="Logo Dicoding"
+          />
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Profile
--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import React from 'react';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Profile from "./Profile.jsx";
 
-//variable di jsx 
-const element = (
-  <div>
-    <h1>Biodata Perusahaan</h1>
-    <ul>
-      <li>Nama: Dicoding Indonesia</li>
-      <li>Bidang: Education</li>
-      <li>Tagline: Decode Ideas, Discover Potential.</li>
-      <img src="https://raw.githubusercontent.com/dicodingacademy/BelajarDasarPemrogramanWeb/099-shared-files/dicoding-logo.png" alt="Logo Dicoding" />
-    </ul>
-  </div>
-);
-
-// Gabungkan App dan container
-const combinedContent = React.createElement('div', null, [
-  React.createElement(App, null), // Komponen App
-  element
-]);
-
-// Render ke DOM
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {combinedContent}
-  </StrictMode>
-);
+    <App />
+    <Profile/>
+  </StrictMode>,
+)
