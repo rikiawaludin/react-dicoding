414cb11cdcad827728f492ca71785d0380ba7a8a
Riki Awaludin <rikiawaludin17@gmail.com>
2024-12-26 13:21:13 +0700

    tambahkan berita

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Profile from "./Profile.jsx";
+import News from './Berita.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <Profile/>
+    <News/>
   </StrictMode>,
 )
