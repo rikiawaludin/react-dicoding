da5412157f56d01029827e7fc59e309d44e5982f
Riki Awaludin <rikiawaludin17@gmail.com>
2024-12-26 21:09:39 +0700

    nambah halaman daftar kontak

---
+++ src/daftarKontak/ContactApp.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+import ContactList from "./ContactList";
+import { getData } from "./data";
+
+export default function ContactApp() {
+  const contacts = getData();
+
+  return (
+    <div className="contact-app">
+      <h1>Daftar Kontak</h1>
+      <ContactList contacts={contacts} />
+    </div>
+  );
+}
