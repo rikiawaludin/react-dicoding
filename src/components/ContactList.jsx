f74fa65901356346ddb748d64ddfd827769f3d7f
Riki Awaludin <rikiawaludin17@gmail.com>
2024-12-27 19:45:28 +0700

    menambahkan fitur hapus dengan state, event handling

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,17 @@
 // import React from 'react';
-import ContactItem from './ContactItem';
- 
-export default function ContactList({ contacts }) {
- return (
-   <div className="contact-list">
-     {
-       contacts.map((contact) => (
-         <ContactItem key={contact.id} {...contact} />
-       ))
-     }
-   </div>
- );
+import ContactItem from "./ContactItem";
+
+export default function ContactList({ contacts, onDelete }) {
+  return (
+    <div className="contact-list">
+      {contacts.map((contact) => (
+        <ContactItem
+          key={contact.id}
+          id={contact.id}
+          onDelete={onDelete}
+          {...contact}
+        />
+      ))}
+    </div>
+  );
 }
